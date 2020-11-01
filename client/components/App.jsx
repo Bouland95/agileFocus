c03144a4e5e7ada31dc477edd6771972e1d418ec
Aziz Bouland <q8boland@gmail.com>
2020-11-02 01:53:44 +0300

    breaking App into smaller components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Section from './Section.jsx';
 
 export class App extends React.Component {
   render() {
@@ -6,33 +7,10 @@ export class App extends React.Component {
       <div>
         <div className="view">
           <div className="viewItem">Tasks</div>
-          <div className="viewItem">Graphs</div>
+          <div className="viewItem">Stats</div>
         </div>
 
-        <div className="projectsContainer">
-          <div className="project">
-            <div className="projectTitle">Project 1</div>
-              <div className="task">
-                some task to be completed
-                <img src="https://img.icons8.com/flat_round/64/000000/play--v1.png" height="20"/>
-                <span className="completeBtn">Complete</span>
-              </div>
-              <div className="task">another task about something
-              <img src="https://img.icons8.com/flat_round/64/000000/play--v1.png" height="20"/>
-              <span className="completeBtn">Complete</span>
-            </div>
-            <div className="projectTitle">Project 2</div>
-              <div className="task">
-                some task to be completed
-                <img src="https://img.icons8.com/flat_round/64/000000/play--v1.png" height="20"/>
-                <span className="completeBtn">Complete</span>
-              </div>
-              <div className="task">another task about something
-              <img src="https://img.icons8.com/flat_round/64/000000/play--v1.png" height="20"/>
-              <span className="completeBtn">Complete</span>
-            </div>
-          </div>
-        </div>
+        <Section />
       </div>
     );
   }
---
+++ client/components/Section.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const Section = () => {
+  return (
+    <div className="section">
+        <h2>BACKLOG</h2>
+        <div className="ticket">
+
+          <div className="ticketName">Implement a new user interface</div>
+
+          <div className="minorTicket">
+            Add drop down menus for project selection
+            <img src="https://img.icons8.com/flat_round/64/000000/play--v1.png" height="20"/>
+            <span className="pushBtn">Push</span>
+          </div>
+
+          <div className="minorTicket">
+            Make it possible to add sub ticket
+            <img src="https://img.icons8.com/flat_round/64/000000/play--v1.png" height="20"/>
+            <span className="pushBtn">Push</span>
+          </div>
+
+          <div className="minorTicket">
+            Allow creation of new tickets
+            <img src="https://img.icons8.com/flat_round/64/000000/play--v1.png" height="20"/>
+            <span className="pushBtn">Push</span>
+          </div>
+
+        </div>
+    </div>
+  )
+}
